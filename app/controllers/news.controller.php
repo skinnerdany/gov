c99cb83d9dc82a72164f7a0d290a5359a99f7cf1
DannyM1chael <coldsoulrusirk@gmail.com>
2020-05-28 15:26:46 +0300

    add denis controller, model, view

--- app/controllers/news.controller.php
+++ app/controllers/news.controller.php
@@ -1,5 +1,19 @@
 <?php
 
 class ControllerNews extends controller {
-    
+    protected $layoutFile = 'newsLayout';
+    protected $error = '';
+    protected $message = '';
+
+    public function actionAdd(){
+       
+    }
+
+    public function actionUpdate(){
+        
+    }
+
+    public function actionDelete(){
+        
+    }
 }
